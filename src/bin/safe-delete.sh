e6964ec59d4861cd39971cf384830e80d36bb6b6
adeposting <217110652+adeposting@users.noreply.github.com>
2025-06-21 19:02:55 -0700

    Add 'safe-delete.sh', a lib and cli for securely deleting files and dirs

---
+++ src/bin/safe-delete.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -oue pipefail
+
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "$CWD/debug.sh"
+source "$CWD/log.sh"
+source "$CWD/package-install.sh"
+
+_help() {
+    echo
+    echo "safe-delete.sh"
+    echo
+    echo "  Usage: safe-delete <path>..."
+    echo
+    echo "Behavior:"
+    echo "  Securely deletes each specified file or directory using the most secure method available:"
+    echo "    - srm (secure remove), if available"
+    echo "    - shred, if available"
+    echo "    - rm -rf, as a fallback"
+    echo
+    echo "Integration:"
+    echo "  You can source this script to reuse the delete function:"
+    echo "    source /path/to/safe-delete.sh"
+    echo
+    echo "  This will make the following function available:"
+    echo "    safe_delete  → securely delete files or directories"
+    echo
+}
+
+
+safe_delete() {
+  package_install srm || package_install shred
+
+  for target in "$@"; do
+    if command -v srm &>/dev/null; then
+      log_info "Securely deleting $target using srm..."
+      srm -fzv "$target"
+    elif command -v shred &>/dev/null; then
+      log_info "Securely shredding $target using shred..."
+      shred -uz "$target"
+    else
+      log_info "Fallback, deleting $target with rm -rf"
+      rm -rf "$target"
+    fi
+  done
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  safe_delete "$@"
+else
+  export -f delete
+fi
